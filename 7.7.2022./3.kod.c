de82118596d4eb4154c898117fe0c72900d00daa
iglibic <ivanglibic77@gmail.com>
2025-08-29 21:14:29 +0200

    Update 3.kod.c

--- 7.7.2022./3.kod.c
+++ 7.7.2022./3.kod.c
@@ -1,14 +1,14 @@
-/*Napisati program koji iz datoteke čita prezime i ime studenta i svakom studentu se dodjeljuje jedninstven broj (ID) u 
-opsegu od 100 do 199 (voditi računa da broj bude jedinstven, tj. da se ne ponavlja). Od dobivenih podataka (ID-a, prezimena i imena) 
-se gradi lista koja je sortirana po ID-u. Pri tome koristi sortirani unos, a ne sortiranje liste. Ispisati listu.
+/*Za ocjenu 2:
+Napisati program koji iz datoteke čita prezime i ime studenta i svakom studentu se dodjeljuje
+jedninstven broj (ID) u opsegu od 100 do 199 (voditi računa da broj bude jedinstven, tj. da se ne
+ponavlja). Od dobivenih podataka (ID-a, prezimena i imena) se gradi lista koja je sortirana po ID-u.
+Pri tome koristi sortirani unos, a ne sortiranje liste. Ispisati listu.
 
-Za ocjenu 2:
-Iz liste ukloniti sve one osobe čije se ime ponavlja u listi, tako da ostane samo jedna osoba tog imena. Npr. ukoliko u listi 
-postoji 3 Ivana, nakon brisanja ostaje samo jedan. Ispisati novu listu.
+Iz liste ukloniti sve one osobe čije se ime ponavlja u listi, tako da ostane samo jedna osoba tog imena.
+Npr. ukoliko u listi postoji 3 Ivana, nakon brisanja ostaje samo jedan. Ispisati novu listu.
 
 Za ocjenu 3:
-Unijeti neki broj i sve one članove liste čiji je ID djeljiv s tim brojem postaviti na kraj liste.
-*/
+Unijeti neki broj i sve one članove liste čiji je ID djeljiv s tim brojem postaviti na kraj liste.*/
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
@@ -16,6 +16,7 @@ Unijeti neki broj i sve one članove liste čiji je ID djeljiv s tim brojem post
 #include <string.h>
 #include <time.h>
 
+#define FILE_NOT_OPENED (-1)
 #define MAX_NAME_LEN (32)
 
 typedef struct _osoba* OsobaP;
@@ -26,16 +27,17 @@ typedef struct _osoba {
     OsobaP next;
 } Osoba;
 
-int generateUniqueId(int usedValues[]);
-OsobaP createNewPerson(char* fName, char* lName, int iD);
-int loadFromFile(OsobaP head, const char* fileName, int usedValues[]);
+int generateUniqueValue(int usedValues[]);
+OsobaP createNewPerson(char* fName, char* lName, int id);
+int loadFromFile(OsobaP head, const char* fileNamem, int usedValues[]);
 int insertSorted(OsobaP head, OsobaP newPerson);
 int printList(OsobaP first);
 int freeList(OsobaP head);
 
-int removeSameName(OsobaP head);
+int removeRepeatName(OsobaP head);
+
 OsobaP findLast(OsobaP head);
-int moveToBackByID(OsobaP head, int ID);
+int moveDividableByNum(OsobaP head, int id);
 
 int main() {
     srand((unsigned int)time(NULL));
@@ -51,19 +53,23 @@ int main() {
 
     loadFromFile(head, "STUDENTI1.txt", usedValues);
 
+    printf("Pocetna lista:\n");
     printList(head->next);
 
-    printf("\nUklanjamo osobe s istim imenima (ostaje samo jedna osoba s tim imenom): \n");
-    removeSameName(head);
+    printf("\nUklanjanje istih imena...");
+    removeRepeatName(head);
 
+    printf("\nKonacna lista:\n");
     printList(head->next);
 
     int id;
 
-    printf("\nUnesite broj: ");
+    printf("\nUnesite vrijednost s kojom dijelimo: ");
     scanf("%d", &id);
-    moveToBackByID(head, id);
 
+    moveDividableByNum(head, id);
+
+    printf("\nLista poslije promjena:\n");
     printList(head->next);
 
     freeList(head);
@@ -71,7 +77,7 @@ int main() {
     return EXIT_SUCCESS;
 }
 
-int generateUniqueId(int usedValues[]) {
+int generateUniqueValue(int usedValues[]) {
     int value;
     do {
         value = rand() % 100 + 100;
@@ -80,35 +86,35 @@ int generateUniqueId(int usedValues[]) {
     return value;
 }
 
-OsobaP createNewPerson(char* fName, char* lName, int iD) {
-    OsobaP newOsoba = (OsobaP)malloc(sizeof(Osoba));
-    if (newOsoba == NULL) {
+OsobaP createNewPerson(char* fName, char* lName, int id) {
+    OsobaP newPerson = (OsobaP)malloc(sizeof(Osoba));
+    if (newPerson == NULL) {
         printf("ERROR! Could not allocate memmory!\n");
         return NULL;
     }
-    strcpy(newOsoba->firstname, fName);
-    strcpy(newOsoba->lastname, lName);
-    newOsoba->id = iD;
+    strcpy(newPerson->firstname, fName);
+    strcpy(newPerson->lastname, lName);
+    newPerson->id = id;
+    newPerson->next = NULL;
 
-    return newOsoba;
+    return newPerson;
 }
 
 int loadFromFile(OsobaP head, const char* fileName, int usedValues[]) {
     FILE* fp = fopen(fileName, "r");
     if (fp == NULL) {
-        printf("ERROR! File could not be opened!\n");
+        printf("ERROR! Could not open the file!\n");
         exit(EXIT_FAILURE);
     }
     char fname[MAX_NAME_LEN], lname[MAX_NAME_LEN];
-
-    while (fscanf(fp, "%s %s ", lname, fname) == 2) {
-        int id = generateUniqueId(usedValues);
-        OsobaP newOsoba = createNewPerson(fname, lname, id);
-        if (newOsoba == NULL) {
+    while (fscanf(fp, "%s %s", lname, fname) == 2) {
+        int id = generateUniqueValue(usedValues);
+        OsobaP newPerson = createNewPerson(fname, lname, id);
+        if (newPerson == NULL) {
             printf("ERROR! Could not allocate memmory!\n");
             return EXIT_FAILURE;
         }
-        insertSorted(head, newOsoba);
+        insertSorted(head, newPerson);
     }
     fclose(fp);
 
@@ -118,9 +124,8 @@ int loadFromFile(OsobaP head, const char* fileName, int usedValues[]) {
 int insertSorted(OsobaP head, OsobaP newPerson) {
     OsobaP prev = head;
     OsobaP curr = head->next;
-
     while (curr) {
-        if (newPerson->id < curr->id) {
+        if (newPerson->id <= curr->id) {
             break;
         }
         else {
@@ -137,7 +142,7 @@ int insertSorted(OsobaP head, OsobaP newPerson) {
 int printList(OsobaP first) {
     OsobaP temp = first;
     while (temp) {
-        printf("ID: %d, Prezime: %s, Ime: %s\n", temp->id, temp->lastname, temp->firstname);
+        printf("Osoba: %s %s, ID: %d\n", temp->firstname, temp->lastname, temp->id);
         temp = temp->next;
     }
     return EXIT_SUCCESS;
@@ -155,11 +160,10 @@ int freeList(OsobaP head) {
     return EXIT_SUCCESS;
 }
 
-int removeSameName(OsobaP head) {
+int removeRepeatName(OsobaP head) {
     OsobaP outer = head->next;
-
     while (outer) {
-        OsobaP prev = outer;                            //outer je taj kojeg zadrazavamo, moze se desiti da izbrises outer
+        OsobaP prev = outer;                                        //odradis ime, ostavis ga i ides na iduce drugacije
         OsobaP curr = outer->next;
         while (curr) {
             if (strcmp(outer->firstname, curr->firstname) == 0) {
@@ -178,22 +182,22 @@ int removeSameName(OsobaP head) {
 }
 
 OsobaP findLast(OsobaP head) {
-    OsobaP temp = head;
+    OsobaP temp = head->next;
     while (temp->next) {
         temp = temp->next;
     }
     return temp;
 }
 
-int moveToBackByID(OsobaP head, int ID) {
+int moveDividableByNum(OsobaP head, int id) {
     OsobaP prev = head;
     OsobaP curr = head->next;
     OsobaP last = findLast(head);
 
     OsobaP originalLast = last;
 
-    while (curr != originalLast) {
-        if ((curr->id % ID) == 0) {
+    while (curr && curr != originalLast) {
+        if ((curr->id % id) == 0) {
             prev->next = curr->next;
             last->next = curr;
             curr->next = NULL;
