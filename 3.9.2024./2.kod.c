1a810d81cd5313b3bdcefa6d70807950cf1528f3
iglibic <ivanglibic77@gmail.com>
2025-08-31 09:45:53 +0200

    Update 2.kod.c

--- 3.9.2024./2.kod.c
+++ 3.9.2024./2.kod.c
@@ -1,19 +1,22 @@
-/*Napisati program koji stvara dvije vezane liste, svaku od 20 elemenata, na način da se za svaki element 
-liste generiraju dvije vrijednosti. Prva je vrijednost u opsegu od 100–130 (i u listi se ne smiju ponavljati 
-vrijednosti), a druga je prioritet u opsegu od 1–5 (bit će ponavljanja). Elementi u listi moraju biti 
+/*Napisati program koji stvara dvije vezane liste, svaku od 20 elemenata, na način da se za svaki element
+liste generiraju dvije vrijednosti. Prva je vrijednost u opsegu od 100–130 (i u listi se ne smiju ponavljati
+vrijednosti), a druga je prioritet u opsegu od 1–5 (bit će ponavljanja). Elementi u listi moraju biti
 sortirani prvo po prioritetu, pa po vrijednosti.
 
 Ocjena 2:
-Ispisati vezanu listu i pronaći vrijednosti koje se ponavljaju u obje liste (uspoređuje se po value 
+Ispisati vezanu listu i pronaći vrijednosti koje se ponavljaju u obje liste (uspoređuje se po value
 elementu), te ispisati te vrijednosti uz njihov prioritet i nakon toga ih izbrisati iz liste.
 Ispisati nove liste.*/
 
-#define _CRT_SECURE_NOWARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
+#define FILE_NOT_OPENED (-1)
+
+struct _data;
 typedef struct _data* DataP;
 typedef struct _data {
     int value;
@@ -21,15 +24,15 @@ typedef struct _data {
     DataP next;
 } Data;
 
-DataP createNewData(int val, int pr);
-int generateUniqueValue(int usedValues[]);
 int generatePriority();
+int generateUniqueValue(int usedValues[]);
+DataP createNewElement(int value, int prioritiy);
 int insertSorted(DataP head, int value, int priority);
 int printList(DataP first);
 int freeList(DataP head);
 
 int findSameValue(DataP head1, DataP head2);
-int deleteSameValue(DataP head, int value);
+int deleteElement(DataP head, int value);
 
 int main() {
     srand((unsigned int)time(NULL));
@@ -49,22 +52,26 @@ int main() {
 
     for (int i = 0; i < 20; i++) {
         insertSorted(head1, generateUniqueValue(usedValues1), generatePriority());
-        insertSorted(head2, generateUniqueValue(usedValues2), generatePriority());
     }
 
-    printf("Prva lista: \n");
+    for (int i = 0; i < 20; i++) {
+        insertSorted(head2, generateUniqueValue(usedValues2), generatePriority());
+    }
+    printf("Lista 1: \n");
     printList(head1->next);
 
-    printf("\nDruga lista: \n");
+    printf("\nLista 2: \n");
     printList(head2->next);
 
-    printf("Trazim vrijednosti koje su se pojavile u obe liste...\n");
+    printf("\nTrazimo vrijednosti koje se nalaze u obje liste...\n");
     findSameValue(head1, head2);
 
-    printf("Konacna prva lista: \n");
+    printf("\nBrisanje istih vrijednostii...\n");
+
+    printf("\nKonacna lista 1: \n");
     printList(head1->next);
 
-    printf("\nKonacna druga lista: \n");
+    printf("\nKonacna lista 2: \n");
     printList(head2->next);
 
     freeList(head1);
@@ -73,41 +80,47 @@ int main() {
     return EXIT_SUCCESS;
 }
 
-DataP createNewData(int val, int pr) {
-    DataP newData = (DataP)malloc(sizeof(Data));
-    if (newData == NULL) {
-        printf("ERROR! Could not allocate memmory!\n");
-        return NULL;
-    }
-    newData->value = val;
-    newData->priority = pr;
-    newData->next = NULL;
-
-    return newData;
+int generatePriority() {
+    return rand() % 5 + 1;
 }
 
 int generateUniqueValue(int usedValues[]) {
     int value;
     do {
-        value = rand() % 31 + 100;
+        value = rand() % (130 - 100 + 1) + 100;
     } while (usedValues[value - 100]);
     usedValues[value - 100] = 1;
     return value;
 }
 
-int generatePriority() {
-    return rand() % 5 + 1;
+DataP createNewElement(int value, int prioritiy) {
+    DataP newElement = (DataP)malloc(sizeof(Data));
+    if (newElement == NULL) {
+        printf("ERROR! Could not allocate memmory!\n");
+        return NULL;
+    }
+    newElement->value = value;
+    newElement->priority = prioritiy;
+    newElement->next = NULL;
+
+    return newElement;
 }
 
-int insertSorted(DataP head, int value, int priority) {
+int insertSorted(DataP head, int value, int priority){
+    DataP newElement = createNewElement(value, priority);
+    if (newElement == NULL) {
+        printf("ERROR! Could not allocate memmory!\n");
+        return EXIT_FAILURE;
+    }
+
     DataP prev = head;
     DataP curr = head->next;
     while (curr) {
-        if (priority < curr->priority) {
+        if (newElement->priority < curr->priority) {
             break;
         }
         else {
-            if ((priority == curr->priority) && (value <= curr->value)) {
+            if ((newElement->priority == curr->priority) && (newElement->value <= curr->value)) {
                 break;
             }
             else {
@@ -116,17 +129,11 @@ int insertSorted(DataP head, int value, int priority) {
             }
         }
     }
-    DataP newData = createNewData(value, priority);
-    if (newData == NULL) {
-        printf("ERROR! Could not allocate memmory!\n");
-        return EXIT_FAILURE;
-    }
-    prev->next = newData;
-    newData->next = curr;
+    prev->next = newElement;
+    newElement->next = curr;
 
     return EXIT_SUCCESS;
 }
-
 int printList(DataP first) {
     DataP temp = first;
     while (temp) {
@@ -152,28 +159,31 @@ int findSameValue(DataP head1, DataP head2) {
     DataP temp1 = head1->next;
     while (temp1) {
         DataP temp2 = head2->next;
+        int found = 0;
+
         while (temp2) {
             if (temp1->value == temp2->value) {
-                printf("Vrijednost: %d se ponavalja u obe liste.\n", temp1->value);
-                deleteSameValue(head1, temp1->value);
-                deleteSameValue(head2, temp2->value);
-                temp1 = head1;                              //obrises temp1 i ne postoji ti pa ga treba opet inicijalizirati
-                                                            //temp2 ne treba jer u novom krugu whilea ide nova vrijednost
-                break;                                      //trenutni while prekines jer si nasao tu vrijednost i ides dalje
+                printf("Vrijednost: %d, Prioritet u listi 1: %d, Prioritet u listi 2: %d\n", temp1->value, temp1->priority, temp2->priority);
+                deleteElement(head1, temp1->value);
+                deleteElement(head2, temp2->value);
+                found = 1;
+                break;
             }
-            else {
-                temp2 = temp2->next;
-            }
+            temp2 = temp2->next;
         }
-        temp1 = temp1->next;
+        if (found) {
+            temp1 = head1->next;
+        }
+        else {
+            temp1 = temp1->next;
+        }
     }
     return EXIT_SUCCESS;
 }
 
-int deleteSameValue(DataP head, int value) {
+int deleteElement(DataP head, int value) {
     DataP prev = head;
     DataP curr = head->next;
-
     while (curr) {
         if (curr->value == value) {
             prev->next = curr->next;
